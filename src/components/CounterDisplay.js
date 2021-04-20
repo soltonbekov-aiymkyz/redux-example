50d7a0fdec0c0012abeb18552e65dce3d29c3826
Your Name <you@example.com>
2021-04-20 03:49:38 -0700

    added and corrected thes files

--- src/components/CounterDisplay.js
+++ src/components/CounterDisplay.js
@@ -1,17 +1,30 @@
 
+// import { useSelector } from "react-redux";
+// const initialState = {
+//   number: 0,
+// };
+// const CounterDisplay = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "increment_by_1":
+//       return { ...state, number: state.number + 1 };  
+//       case "decriment_by_1":
+//         return { ...state, number: state.number - 1 };    
+//   }
+//   return state;
+// }
+// export default CounterDisplay;
+
 
 import { useSelector } from "react-redux";
 
-const initialState = {
-  number: 0,
-};
-const CounterDisplay = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment_by_1":
-      return { ...state, number: state.number + 1 };  
-      case "decriment_by_1":
-        return { ...state, number: state.number - 1 };    
-  }
-  return state;
+const CounterDisplay = () => {
+    const number = useSelector(state => state.number);
+
+    console.log(number)
+
+    return ( 
+        <h1>{number}</h1>
+    );
 }
+ 
 export default CounterDisplay;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,16 +1,30 @@
+// const initialState = {
+//     number: 0,
+//   };
+//   const Reducers = (state = initialState, action) => {
+//     switch (action.type) {
+//       case "increment_by_1":
+//         return { ...state, number: state.number + 1 };
+//         case "decrement_by_1":
+//             return { ...state, number: state.number - 1 };
+//     }
+//     return state;
+//   }
+//   export default Reducers;
+
 const initialState = {
     number: 0,
-  };
-  const Reducers = (state = initialState, action) => {
+}
+
+const counterReducer = (state = initialState, action) => {
     switch (action.type) {
-      case "increment_by_1":
-        return { ...state, number: state.number + 1 };
+        case "increment_by_1":
+            return { ...state, number: state.number + 1 }
         case "decrement_by_1":
-            return { ...state, number: state.number - 1 };
-    
-
-
+            return { ...state, number: state.number - 1 }
     }
+    
     return state;
-  }
-  export default Reducers;
+}
+
+export default counterReducer;
