3ccc2a2c65e0fe20a8b809a5e3370ed723e721f6
Your Name <you@example.com>
2021-04-21 00:42:17 -0700

    added new styles for this page

--- src/components/CounterSimpleControls.js
+++ src/components/CounterSimpleControls.js
@@ -1,18 +1,59 @@
 
 import { useDispatch } from "react-redux";
+
 const CounterSimpleControls = () => {
+
   const dispatch = useDispatch();
+
   function incrementBy1Callback() {
     dispatch({ type: "increment_by_1" });
   }
   function decrementBy1Callback() {
     dispatch({ type: "decrement_by_1" });
   }
+
+  function incrementBy10Callback() {
+    dispatch({ type: "increment_by_10" });
+  }
+  
+  function decrementBy10Callback() {
+    dispatch({ type: "decrement_by_10" });
+  }
+
+  function incrementBy100Callback() {
+    dispatch({ type: "increment_by_100" });
+  }
+  function decrementBy100Callback() {
+    dispatch({ type: "decrement_by_100" });
+  }
+
+
+
+
+
+
   return (
     <div>
       <button onClick={incrementBy1Callback}>+1</button>
       <button onClick={decrementBy1Callback}>-1</button>
+
+
+
+      <button onClick={incrementBy10Callback}>+10</button>
+      <button onClick={decrementBy10Callback}>-10</button>
+    
+   
+      <button onClick={incrementBy100Callback}>+100</button>
+      <button onClick={decrementBy100Callback}>-100</button>
+    
+
+
     </div>
   );
 };
 export default CounterSimpleControls;
+
+
+
+
+
