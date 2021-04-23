9eba4a85131a813b2f121a2a422ecf93d767bbc4
Your Name <you@example.com>
2021-04-22 22:44:02 -0700

    added new something

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import CounterDisplay from './components/CounterDisplay';
 import CounterSimpleControls from './components/CounterSimpleControls';
 
 import IncericeSimple from './components/IncericeSimple';
+
+import CounterAdvancedControls from './components/CounterAdvancedControls'
+import ColorDisplay from './components/ColorDisplay'
+
+import SliderSimple from './components/SliderSimple'
+import ColorSimple from './components/ColorSimple'
 function App() {
   return (
     <div className="App">
+      <ColorDisplay />
+      <ColorSimple />
+      <SliderSimple />
       <CounterDisplay />
       <CounterSimpleControls />
-
       <IncericeSimple />
+      <CounterAdvancedControls />
     </div>
   );
 }
