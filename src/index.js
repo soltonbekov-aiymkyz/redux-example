90cbb3ce0b947ef8a678a44220338b2349a91188
Your Name <you@example.com>
2021-04-22 22:46:41 -0700

    adde some phtotos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider, ReactReduxContext } from 'react-redux';
+// import { createStore, combineReducers } from 'redux';
+// import App from './App';
+
+
+// import reportWebVitals from './reportWebVitals'
+
+// const store = createStore();
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
 import React from 'react';
+import { createStore, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './App';
-import counterReducer from './redux/reducers';
-import reportWebVitals from './reportWebVitals'
-const store = createStore(counterReducer);
+import counterReducers from '../src/redux/counterReducers';
+import colorReduser from '../src/redux/colorReduser';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
+const root = combineReducers({ counterReducers, colorReduser })
+const store = createStore()
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,12 +45,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
 
 
