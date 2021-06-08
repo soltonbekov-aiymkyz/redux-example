1a20cdf53b02e2bf138f1833cc8031f41de2ab4f
Your Name <you@example.com>
2021-06-07 22:46:00 -0700

    added new changes for thhis pahe

--- src/redux/aiymRedusers.js
+++ src/redux/aiymRedusers.js
@@ -1,25 +1,16 @@
 
 const initialState = {
     text: "",
-    size: 16,
-     name:"aiymkyz",
 }
-const textRedusers = (state = initialState, action)  => {
+const aiymRedusers = (state = initialState, action)  => {
     const newState = { ...state };
     switch (action.type) {
-        case "set_text":
+        case "text":
             newState.text = action.text;
             return newState;
-        case "size":
-            newState.size = action.size;
-            return newState;
-
+        
           
-
-            case "toUpperCase":
-                newState.text = action.text;
-                return newState;
     }  
     return state;
 }
-export default textRedusers;
+export default aiymRedusers;
